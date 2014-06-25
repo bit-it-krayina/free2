da84e07f7602ee06c5a6cfead7f755bbf792f83d
mirantel <magi.eventail@gmail.com>
2014-06-25 17:17:15 +0300

    BIT-28 - [Верстка] Вывод сообщений для ошибок регистрации - логина

--- public/js/mine/profile.js
+++ public/js/mine/profile.js
@@ -26,77 +26,44 @@ function savePicture(form) {
 }
 
 $(document).ready(function(){
-//	$('.js-tagsinput').tagsinput({
-//		typeahead{
-//			source: ['sql', 'symphony', 'mysql', 'mysqli']
-//			source: function(query) {
-//				console.log('123');
-//				return $.getJSON('/profile-ajax/getAvailableTags');
-//			}
-//		}
-//	});
 
-	// $('.js-tagsinput').tagsinput({
-	// 	typeahead: {
-	// 		source: function(query) {
-	// 			return $.getJSON('getAvailableTags.json');
-	// 		}
-	// 	}
-	// });
+	/**
+	 * автодополнение тегов
+	 */
+	var substringMatcher = function(strs) {
+		return function findMatches(q, cb) {
+			var matches, substringRegex;
 
+			matches = [];
+			substrRegex = new RegExp(q, 'i');
 
+			$.each(strs, function(i, str) {
+				if (substrRegex.test(str)) {
+					matches.push({ value: str });
+				}
+			});
 
-// var substringMatcher = function(strs) {
-//   return function findMatches(q, cb) {
-//     var matches, substringRegex;
+			cb(matches);
+		};
+	};
 
-//     matches = [];
-//     substrRegex = new RegExp(q, 'i');
-
-//     $.each(strs, function(i, str) {
-//       if (substrRegex.test(str)) {
-//         matches.push({ value: str });
-//       }
-//     });
-
-//     cb(matches);
-//   };
-// };
-
-// var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-// ];
+	var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+	  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+	  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+	  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+	  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+	  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+	  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+	  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+	  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+	];
 
 
-// 	$('.js-tagsinput').tagsinput();
-// 	$('.bootstrap-tagsinput input').typeahead({} ,{
-// 		source: substringMatcher(states)
-// 	});
-
-
-	//$('.js-tagsinput').tagsinput();
-
-	var myTags = new Bloodhound({
-		datumTokenizer: Bloodhound.tokenizers.obj.whitespace('tags'),
-		queryTokenizer: Bloodhound.tokenizers.whitespace,
-		prefetch: '/js/countries.json'
+	$('.js-tagsinput').tagsinput();
+	$('.bootstrap-tagsinput input').typeahead({} ,{
+		source: substringMatcher(states)
 	});
 
-	myTags.initialize();
-
-	$('.js-tagsinput').typeahead(
-		{},
-		{
-			source: myTags.ttAdapter()
-		}
-	);
 
 
 	$('.js-datepicker').each(function(){
